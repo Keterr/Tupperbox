3d0529268d61294c80d68543fe00a10aba3ebfe2
Aries <sinistarrrs@gmail.com>
2020-08-21 22:49:24 -0700

    Formatted usage descriptions

--- commands/cfg.js
+++ commands/cfg.js
@@ -2,11 +2,12 @@ const {article,proper} = require("../modules/lang");
 
 module.exports = {
 	help: cfg => "Configure server-specific settings",
-	usage: cfg =>  ["cfg prefix <newPrefix> - Change the bot's prefix.",
-		"cfg rename <newname> - Change all instances of the default name 'member' in bot replies in this server to the specified term.",
-		"cfg log [channel] - Enable the bot to send a log of all " + cfg.lang + " messages and some basic info like who registered them. Useful for having a searchable channel and for distinguishing between similar names. To disable logging, run with no channel argument.",
-		"cfg blacklist <add|remove> <channel(s)> - Add or remove channels to the bot's proxy blacklist - users will be unable to proxy in blacklisted channels.",
-		"cfg cmdblacklist <add|remove> <channel(s)> - Add or remove channels to the bot's command blacklist - users will be unable to issue commands in blacklisted channels."],
+	usage: cfg =>  [
+		{usage: "cfg prefix", params: "<newPrefix>", description: "Change the bot's prefix."},
+		{usage: "cfg rename", params: "<newname>", description: "Change all instances of the default name 'member' in bot replies in this server to the specified term."},
+		{usage: "cfg log", params: "[channel]", description: `Enable the bot to send a log of all ${cfg.lang} messages and some basic info like who registered them. Useful for having a searchable channel and for distinguishing between similar names. To disable logging, run with no channel argument.`},
+		{usage: "cfg blacklist", params: "<add|remove> <channel(s)>", description: "Add or remove channels to the bot's proxy blacklist - users will be unable to proxy in blacklisted channels."},
+		{usage: "cfg cmdblacklist", params: "<add|remove> <channel(s)>", description: "Add or remove channels to the bot's command blacklist - users will be unable to issue commands in blacklisted channels."}],
 		
 	permitted: (msg) => (msg.member && msg.member.permission.has("administrator")),
 	execute: async (bot, msg, args, cfg) => {
