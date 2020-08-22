1cf33d952c86bc8d06e242686bc8f4612b7ed176
Aries <sinistarrrs@gmail.com>
2020-08-21 23:02:58 -0700

    Formatted usage descriptions and grammatical fixes

--- commands/showuser.js
+++ commands/showuser.js
@@ -1,6 +1,7 @@
 module.exports = {
 	help: cfg => "Show the user that registered the " + cfg.lang + " that last spoke",
-	usage: cfg =>  ["showuser - Finds the user that registered the " + cfg.lang + " that last sent a message in this channel"],
+	usage: cfg =>  [
+		{usage: "showuser", params: "", description: `Finds the user that registered the ${cfg.lang} that last sent a message in this channel.`}],
 	permitted: (msg) => true,
 	execute: (bot, msg, args, cfg) => {
 		if(!bot.recent[msg.channel.id])	return "No " + cfg.lang + "s have spoken in this channel since I last started up, sorry.";
