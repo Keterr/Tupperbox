aed092f6420d8627ec03b514cdb710926cd43991
Aries <sinistarrrs@gmail.com>
2020-08-21 22:59:12 -0700

    Formatted usage descriptions

--- commands/listng.js
+++ commands/listng.js
@@ -1,6 +1,7 @@
 module.exports = {
 	help: cfg => "Like list, but without showing group info.",
-	usage: cfg =>  ["list [user] - Sends a list of the user's registered " + cfg.lang + "s, their brackets, post count, and birthday (if set). If user is not specified it defaults to the message author.\nThe bot will provide reaction emoji controls for navigating long lists: Arrows navigate through pages, # jumps to a specific page, ABCD jumps to a specific " + cfg.lang + ", and the stop button deletes the message."],
+	usage: cfg =>  [
+		{usage: "list", params: "[user]", description: `Sends a list of the user's registered ${cfg.lang}s, their brackets, post count, and birthday (if set). If user is not specified it defaults to the message author.\nThe bot will provide reaction emoji controls for navigating long lists: Arrows navigate through pages, # jumps to a specific page, ABCD jumps to a specific ${cfg.lang}, and the stop button deletes the message.`}],
 	permitted: () => true,
 	cooldown: msg => 60000,
 	execute: async (bot, msg, args, cfg, members) => {
