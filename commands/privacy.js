ba21944c5f0c6d8cc4d83a332449fc9546acf7ed
Aries <sinistarrrs@gmail.com>
2020-08-31 07:38:07 -0700

    Formatting for eventual merge

--- commands/privacy.js
+++ commands/privacy.js
@@ -4,7 +4,7 @@ fs.readFile("./privacy.txt").then(file => policy = file.toString());
 
 module.exports = {
 	help: cfg => "View my privacy policy.",
-	usage: cfg => ["privacy - show the privacy policy"],
+	usage: cfg => [usage: "privacy", params: "", description: "Show the privacy policy"],
 	permitted: msg => true,
 	execute: (bot, msg, args, cfg) => {
 		return policy;
