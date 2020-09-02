ff4793deec8b4b034f7cb7cc6fddf7de81aad8a0
Aries <sinistarrrs@gmail.com>
2020-09-02 15:37:34 -0700

    refactor help embed

--- commands/help.js
+++ commands/help.js
@@ -2,16 +2,16 @@ const {article} = require("../modules/lang");
 
 module.exports = {
 	help: cfg => "Print this message, or get help for a specific command",
-	usage: cfg =>  ["help - print list of commands", 
-		"help [command] - get help on a specific command"],
+	usage: cfg =>  [
+		{usage:"help", params: "", description: "Prints a list of cmds."}, 
+		{usage:"help", params:"[command]", description: "Get help on a specific command."}],
 	permitted: () => true,
-	execute: async (bot, msg, args, cfg) => {
+	command: (bot, command, cfg) => {
 		//help for a specific command
-		if(args[0]) {
-			if(bot.cmds[args[0]] && bot.checkPermissions(bot.cmds[args[0]],msg,args) && bot.cmds[args[0]].usage) {
+			if(bot.cmds[command] && bot.cmds[command].usage){
 				let output = { embed: {
-					title: "Bot Command | " + args[0],
-					description: bot.cmds[args[0]].help(cfg) + "\n\n**Usage:**\n",
+					title: `Bot Command | ${command}`,
+					description: bot.cmds[command].help(cfg) + "\n\n**Usage:**\n",
 					timestamp: new Date().toJSON(),
 					color: 0x999999,
 					author: {
@@ -22,14 +22,22 @@ module.exports = {
 						text: "If something is wrapped in <> or [], do not include the brackets when using the command. They indicate whether that part of the command is required <> or optional []."
 					}
 				}};
-				for(let u of bot.cmds[args[0]].usage(cfg))
+				for(let u of bot.cmds[command].usage(cfg)) {
 					output.embed.description += `**${cfg.prefix}${u.usage}** ${u.params ? `*${u.params}*` : ''} - ${u.description}\n`;
-				if(bot.cmds[args[0]].desc)
-					output.embed.description += `\n${bot.cmds[args[0]].desc(cfg)}`;
-				return output;
+				} 
+				if(bot.cmds[command].desc) {
+					output.embed.description += `\n${bot.cmds[command].desc(cfg)}`;
+				} return output;
 			}
 			return "Command not found.";
-		}
+		},
+	execute: async (bot, msg, args, cfg) => {
+		
+		if (args[0]) {
+			if(bot.cmds[args[0]] && bot.cmds[args[0]].usage && bot.checkPermissions(bot.cmds[args[0]], msg, args)){
+					return module.exports.command(bot, args[0], cfg);
+				} return "Command not found.";
+		} 
 
 		//general help
 		let output = { embed: {
@@ -41,11 +49,12 @@ module.exports = {
 				icon_url: bot.user.avatarURL
 			}
 		}};
-		for(let cmd of Object.keys(bot.cmds)) {
-			if(bot.cmds[cmd].help && bot.cmds[cmd].permitted(msg,args))
-				output.embed.description += `**${cfg.prefix + cmd}**  -  ${bot.cmds[cmd].help(cfg)}\n`;
+		for(let command of Object.keys(bot.cmds)) {
+			if(bot.cmds[command].help && bot.cmds[command].permitted(msg,args))
+				output.embed.description += `**${cfg.prefix + command}**  -  ${bot.cmds[command].help(cfg)}\n`;
 		}
 		output.embed.fields = [{ name: "\u200b", value: "Single or double quotes can be used in any command to specify multi-word arguments!\n\nProxy tips:\nReact with \u274c to a recent proxy to delete it (if you sent it)!\nReact with \u2753 to a recent proxy to show who sent it in DM!\n\nQuestions? Join the support server: [invite](https://discord.gg/rHxMbt2)\nNow accepting donations to cover server costs! [patreon](https://www.patreon.com/tupperbox)\nInvite the bot to your server --> [click](https://discordapp.com/oauth2/authorize?client_id="+process.env.DISCORD_INVITE+"&scope=bot&permissions=536996928)"}];
 		return output;
 	}
-};
+
+	}
