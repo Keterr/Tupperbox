b5255d3093b5d0f89c442dbeb2fe2034df8708a5
dev-kittens <acw@ml1.net>
2020-09-15 22:53:16 -0400

    nicer eval command

--- commands/dev.js
+++ commands/dev.js
@@ -6,18 +6,23 @@ module.exports = {
 		if (msg.author.id != bot.owner) return;
 		switch(args.shift()) {
 		case "eval":
+			let code = args.join(" ");
+			if (e = code.match(/^```js\n([^]*)\n```$/)) code = e[1];
 			let out;
 			try {
-				out = await eval(msg.content.slice(cfg.prefix.length + 9).trim());
+				code.split("\n").length > 1 ? (out = await eval(`(async () => {${code}})();`)) : (out = await eval(code));
 			} catch(e) { out = e.toString(); }
 			return util.inspect(out).split(process.env.DISCORD_TOKEN).join("[[ TOKEN ]]").slice(0,2000);
 		case "reload":
-			process.send({name: "broadcast", msg: {name: "reload", type: args[0], targets: args.slice(1), channel: msg.channel.id}});
+			process.send({name: "broadcast", msg: {name: "reload", type: args[0], targets: args.slice(1)}});
 			if(args[0] == "queue") process.send({name:"reloadQueue"});
 			if(args[0] == "ipc") process.send({name:"reloadIPC"});
 			return "Reload command sent!";
 		case "blacklist":
 			await bot.banAbusiveUser(args.shift(), msg.channel.id);
+			break;
+		case "rawstats":
+			process.send({ name: "rawstats", channelID: msg.channel.id });
 			break;
 		}
 	}
