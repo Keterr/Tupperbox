21d862c12702fd4a255f98035ee33289de769d2d
LyndaFox <jeeninw@gmail.com>
2020-10-03 13:05:21 +0700

    Change help info

--- commands/listr.js
+++ commands/listr.js
@@ -1,8 +1,8 @@
 module.exports = {
-	help: cfg => "Get a detailed list of yours or another user's registered " + cfg.lang + "s",
-	usage: cfg =>  ["list [user] - Sends a list of the user's registered " + cfg.lang + "s, their brackets, post count, and birthday (if set). If user is not specified it defaults to the message author.\nThe bot will provide reaction emoji controls for navigating long lists: Arrows navigate through pages, # jumps to a specific page, ABCD jumps to a specific " + cfg.lang + ", and the stop button deletes the message."],
+	help: cfg => "Get a detailed list of yours or another user's registered " + cfg.lang + "s and relays",
+	usage: cfg =>  ["listr [user] - Sends a list of the user's registered " + cfg.lang + "s and relays, their brackets, post count, and birthday (if set). If user is not specified it defaults to the message author.\n\tlistr ["+ cfg.lang + " name] [user] - Send a list of relay asscociated with the account for a user. If not specified, take message author instead.\nThe bot will provide reaction emoji controls for navigating long lists: Arrows navigate through pages, # jumps to a specific page, ABCD jumps to a specific " + cfg.lang + ", and the stop button deletes the message."],
 	permitted: () => true,
-	cooldown: msg => 60, //60000
+	cooldown: msg => 60000,
 	execute: async (bot, msg, args, cfg, _members, ng = false) => {
 
 		//get target list
